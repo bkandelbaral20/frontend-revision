695077e3452dcee66f45a065c4a8985fe37e8156
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-12 16:43:40 -0500

    prompt

---
+++ js/jsReview.js
@@ -0,0 +1,36 @@
+
+"use strict";
+// console.log('Hello from JavaScript!');
+// alert("what is your name");
+//
+// var confirmed = confirm("Are you sure, you want to exit it ")
+// console.log(confirmed);
+//
+// var userInput = prompt("please type something");
+// console.log("you typed" + userInput);
+
+// function increment(x) {
+//     return x + 1;
+// }
+//
+// var four = increment(3);
+// alert(four);
+// var six = increment(increment(increment(3)));
+// alert(six);
+
+// function increment(someNumber) {
+//     return someNumber + 1;
+//     console.log('you will never see this.');
+//     return someNumber + 2; // This will never run
+// }
+// alert(increment(3));
+
+var pizzaPreference = prompt("What kind of pizza do you like?");
+
+if (pizzaPreference === "pineapple and hot sauce") {
+    alert("What a coincidence, that's my favorite!");
+} else if (pizzaPreference === "cheese") {
+    alert("Just plain cheese? Okay...");
+} else {
+    alert(pizzaPreference + " isn't my favorite, but let's order some!");
+}
