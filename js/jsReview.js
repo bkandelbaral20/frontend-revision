6505f91f982363febcd1fca8520f9f5c8744d2e5
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-12 20:06:19 -0500

    playinh with event listener

--- js/jsReview.js
+++ js/jsReview.js
@@ -29,14 +29,52 @@
 // alert(increment(3));
 
 //if statement
-var pizzaPreference = prompt("What kind of pizza do you like?");
+// var pizzaPreference = prompt("What kind of pizza do you like?");
 
-if (pizzaPreference === "pineapple and hot sauce") {
-    alert("What a coincidence, that's my favorite!");
-} else if (pizzaPreference === "cheese") {
-    alert("Just plain cheese? Okay...");
-} else {
-    alert(pizzaPreference + " isn't my favorite, but let's order some!");
+// if (pizzaPreference === "pineapple and hot sauce") {
+//     alert("What a coincidence, that's my favorite!");
+// } else if (pizzaPreference === "cheese") {
+//     alert("Just plain cheese? Okay...");
+// } else {
+//     alert(pizzaPreference + " isn't my favorite, but let's order some!");
+// }
+
+
+//objects
+
+// this code will produce a console log every second
+// when count >= max, the interval is cancelled, and the logging will stop
+
+// var count = 0;
+// var max = 10;
+// var interval = 1000; // interval time in milliseconds
+//
+// var intervalId = setInterval(function () {
+//     if (count >= max) {
+//         clearInterval(intervalId);
+//        alert('All done');
+//     } else {
+//         count++;
+//         alert('Repeating this line ' + count);
+//     }
+// }, interval);
+//
+
+
+// var delay = 5000; // delay time in milliseconds
+//
+// var timeoutId = setTimeout(function () {
+//     alert('Here is a delayed hello!');
+//     // clearTimeout(timeoutId);
+// }, delay);
+
+const button1 = document.getElementById('btn1');
+
+ const listener = function (e) {
+    alert('You clicked the button!');
 }
+
+// register the listener to handle clicks on btn1
+button1.addEventListener('dblclick', listener);
 
 
